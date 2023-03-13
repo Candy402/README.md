13a4513497d03415a6b05d17753cd2876c9d2e69
Candy402 <126973874+Candy402@users.noreply.github.com>
2023-03-13 14:11:25 +0800

    Add files via upload

---
+++ Assignment_Tool for science.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **NGUYEN THI HUONG<br>
+# DATA SCIENCE**
+
+# *Data science is a crucial and rapidly growing field that combines statistical and computational methods to extract insights and knowledge from data. It involves a diverse range of techniques, including data preprocessing, data analysis, machine learning, and visualization, among others. The applications of data science are vast and varied, from business intelligence and marketing to healthcare and scientific research.*
+
+# ### The following code tests the Gauss formula
+
+# In[2]:
+
+
+def gauss(n):
+    return (n*(n+1))/2
+
+gauss(100) == sum(range(101))
+
+
+# - one
+# - two
+# - three
+# - one
+# - two
+# - three
+
+# | Animal | Description | Image |
+# | --- | --- | --- |
+# | Dog | A furry mammal often kept as a pet. | ![Dog](https://www.google.com/search?q=dog&sxsrf=AJOqlzUDAj4TKDPRLRCgZz1fM-3_5bOHLg:1678504317047&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjT9cjs89L9AhUM0GEKHdLfCPcQ_AUoAXoECAEQAw&biw=1280&bih=595&dpr=1.25#imgrc=M1gdx1TqeiwBWM) |
+# | Cat | A small carnivorous mammal often kept as a pet. | ![Cat](https://www.google.com/search?q=cat&sxsrf=AJOqlzUYtXeX37hrzol6DaDs56932kzCLw:1678504351013&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi07eH889L9AhXBUN4KHaO9BOoQ_AUoAXoECAEQAw&biw=1234&bih=602&dpr=1.25#imgrc=0V922RrJgQc9SM) |
+# | Lion| A large carnivorous feline found in Africa. | ![Lion](https://www.google.com/search?q=lion&tbm=isch&ved=2ahUKEwioxYj-89L9AhViuFYBHcmYBvEQ2-cCegQIABAA&oq=lion&gs_lcp=CgNpbWcQAzIECAAQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoECCMQJzoHCCMQ6gIQJ1DABViyEGDlFWgBcAB4AIABtAGIAe4GkgEDMi41mAEAoAEBqgELZ3dzLXdpei1pbWewAQrAAQE&sclient=img&ei=ofELZOjwLOLw2roPybGaiA8&bih=602&biw=1234#imgrc=A8CLjICQNBI9zM) |
+# | Hyperlinks | Links to other web pages or resources. | [Google](https://www.google.com) |
+# | Code/Syntax Highlighting | Highlights code or programming syntax. | `print("I LOVE YOU")` |
+# | Blocked Quotes | Indents and styles quoted text. | > Practice makes perfect |
+# | Strikethrough | Puts a line through text. | ~~DELETE IT.~~ |
