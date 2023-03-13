c755cce72edf3bc463948a3156f9c13b9bb11821
Candy402 <126973874+Candy402@users.noreply.github.com>
2023-03-14 00:05:17 +0800

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# # Tools for Data Science
+
+# *Tools for data science refer to the various software, programming languages, and frameworks that are used by data scientists and analysts to collect, process, analyze, and visualize data. These tools allow users to extract insights and make data-driven decisions that can improve business operations and solve complex problems.*
+
+# ## Popular Data Science Languages
+# 
+# If you're interested in data science, you'll need to learn at least one programming language. Here are some of the most popular languages used in data science:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+# 5. MATLAB
+
+# ## Popular Data Science Libraries
+# 
+# Data science libraries are an essential part of any data scientist's toolkit. Here are some of the most popular libraries used in data science:
+# 
+# 1. NumPy
+# 2. Pandas
+# 3. Matplotlib
+# 4. Scikit-learn
+# 5. TensorFlow
+
+# ## Data Science Tools
+# 
+# | Tool | Description |
+# | --- | --- |
+# | Python | A versatile programming language for data science. |
+# | R | A programming language designed for statistical computing and graphics. |
+# | SQL | A database management language for working with structured data. |
+# | Jupyter Notebook | An interactive coding environment for data science. |
+# | Tableau | A data visualization and business intelligence tool. |
+# | Apache Hadoop | An open-source framework for distributed storage and processing of big data. |
+# 
+
+# ## Arithmetic Expression Examples
+# 
+# Arithmetic expressions are mathematical expressions that use arithmetic operators such as addition, subtraction, multiplication, and division to evaluate numerical values. Here are some examples of arithmetic expressions:
+
+# In[9]:
+
+
+a = 5
+b = 10
+c = a * b
+d = a + b
+print("The product of {} and {} is {}".format(a, b, c))
+print("The sum of {} and {} is {}".format(a, b, d))
+
+
+# In[10]:
+
+
+minutes = 150
+hours = minutes / 60
+print("{} minutes is equal to {} hours".format(minutes, hours))
+
+
+# ## My Favorite Fruits
+# 
+# I love all kinds of fruits! Here are some of my favorites:
+# 
+# - Strawberries
+# - Mangoes
+# - Pineapples
+# - Watermelons
+# - Apples
+
+# __*AUthor*: NGUYEN THI HUONG__
+
+# ### The following code tests the Gauss formula
+
+# In[14]:
+
+
+def gauss(n):
+    return (n*(n+1))/2
+
+gauss(100) == sum(range(101))
+
+
+# - one
+# - two
+# - three
+# - one
+# - two
+# - three
+
+# | Animal | Description | Image |
+# | --- | --- | --- |
+# | Dog | A furry mammal often kept as a pet. | ![Dog](https://www.google.com/search?q=dog&sxsrf=AJOqlzUDAj4TKDPRLRCgZz1fM-3_5bOHLg:1678504317047&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjT9cjs89L9AhUM0GEKHdLfCPcQ_AUoAXoECAEQAw&biw=1280&bih=595&dpr=1.25#imgrc=M1gdx1TqeiwBWM) |
+# | Cat | A small carnivorous mammal often kept as a pet. | ![Cat](https://www.google.com/search?q=cat&sxsrf=AJOqlzUYtXeX37hrzol6DaDs56932kzCLw:1678504351013&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi07eH889L9AhXBUN4KHaO9BOoQ_AUoAXoECAEQAw&biw=1234&bih=602&dpr=1.25#imgrc=0V922RrJgQc9SM) |
+# | Lion| A large carnivorous feline found in Africa. | ![Lion](https://www.google.com/search?q=lion&tbm=isch&ved=2ahUKEwioxYj-89L9AhViuFYBHcmYBvEQ2-cCegQIABAA&oq=lion&gs_lcp=CgNpbWcQAzIECAAQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoECCMQJzoHCCMQ6gIQJ1DABViyEGDlFWgBcAB4AIABtAGIAe4GkgEDMi41mAEAoAEBqgELZ3dzLXdpei1pbWewAQrAAQE&sclient=img&ei=ofELZOjwLOLw2roPybGaiA8&bih=602&biw=1234#imgrc=A8CLjICQNBI9zM) |
+# | Hyperlinks | Links to other web pages or resources. | [Google](https://www.google.com) |
+# | Code/Syntax Highlighting | Highlights code or programming syntax. | `print("I LOVE YOU")` |
+# | Blocked Quotes | Indents and styles quoted text. | > Practice makes perfect |
+# | Strikethrough | Puts a line through text. | ~~DELETE IT.~~ |
+
+# In[ ]:
+
+
+
+
